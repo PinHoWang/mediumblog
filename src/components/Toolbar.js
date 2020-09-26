b78179b320c47e402bc7fd34dfb5347e8d2c0fc9
PinHoWang <abc8755987@gmail.com>
2020-09-26 15:23:55 -0400

    updated the navbar

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,22 +1,34 @@
 import React, { Component } from "react";
-import '../App.css'
+import "../App.css";
 
 import logo from "./assets/temp_logo.png";
+import photo from "./assets/photo.png";
 
 class Toolbar extends Component {
-    render() {
-        return (
-            <div className='toolbar'>
-                <a href="/">
-                    <img src={logo} alt="logo" />{" "}
-                </a>
-                <ul>
-                    <li><a href='/'>Blog</a></li>
-                    <li><a href='/newblog'>Post</a></li>
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="toolbar">
+        <div className="toolbar-left">
+          <a href="/">
+            <img src={logo} alt="logo" />{" "}
+          </a>
+          <span>Skunk.ai Researcher Blog</span>
+          {/* <ul>
+            <li>
+              <a href="/">Blog</a>
+            </li>
+            <li>
+              <a href="/newblog">Post</a>
+            </li>
+          </ul> */}
+
+        </div>
+        <div className="toolbar-right">
+          <img src={photo} alt="profile" />{" "}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Toolbar;
